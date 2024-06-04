e363bcc2a7af4767ff1c7bbe276d1a197b9ffdfd
mloherr <herraizmaria08@gmail.com>
2024-06-04 10:37:17 +0200

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+    children: [
+      {
+        path: 'quizz',
+        loadComponent: () => import ('./components/quizz/quizz.component').then(response => response.QuizzComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];

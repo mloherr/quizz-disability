458aa506485b384a9c078e90aa6dc45aaf428376
mloherr <herraizmaria08@gmail.com>
2024-06-10 13:17:47 +0200

    Created quizz service for load questions

---
+++ src/app/services/quizz.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Question {
+  question: string;
+  isCorrect: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  private questionsUrl = 'assets/questions.json'; // Ruta del archivo JSON
+
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<{ questions: Question[] }> {
+    return this.http.get<{ questions: Question[] }>(this.questionsUrl);
+  }
+}

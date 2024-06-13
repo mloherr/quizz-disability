d5ca7417e6675c86e08cbbe44377983e95d979a6
mloherr <herraizmaria08@gmail.com>
2024-06-13 16:53:25 +0200

    Fixed router to return to landing page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })

4b1b9a4fa8bd98992b6bee66b25918b55f37b8e8
mloherr <herraizmaria08@gmail.com>
2024-06-19 19:54:34 +0200

    New structure

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
